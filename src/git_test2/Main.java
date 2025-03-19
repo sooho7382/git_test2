a3233e107d0ae97cb171e2d94f2cca8d15bc3c4a
sooho7382 <sooho7382@gmail.com>
2025-03-19 20:17:08 +0900

    Hello 클래스 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,9 +1,10 @@
-package git_test2;
-
+package git_test2; 
+ 
 public class Main { 
-
+ 
     public static void main(String[] args) { 
-        Hello hello = new Hello(); 
-        hello.say(); 
+        Hello h = new Hello("hello world"); 
+        h.say(); 
     } 
+ 
 }
